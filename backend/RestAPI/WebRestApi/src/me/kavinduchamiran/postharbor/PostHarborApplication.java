39ff1869e98265ff459502ae598fd089d8fa3ad2
kavinduchamiran <kavindu.chamiran@gmail.com>
2023-08-11 21:24:30 +0530

    Initial commit

---
+++ backend/RestAPI/WebRestApi/src/me/kavinduchamiran/postharbor/PostHarborApplication.java
@@ -0,0 +1,15 @@
+package me.kavinduchamiran.postharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class PostHarborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostHarborApplication.class, args);
+	}
+
+}
---
+++ backend/RestAPI/WebRestApi/test/me/kavinduchamiran/postharbor/PostHarborApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kavinduchamiran.postharbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostHarborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
