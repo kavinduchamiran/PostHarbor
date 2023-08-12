2f9e9748100e8439bfff927246f915caeab9593a
kavinduchamiran <kavindu.chamiran@gmail.com>
2023-08-12 07:09:14 +0530

    set security configuration to basic auth

--- backend/RestAPI/WebRestApi/src/me/kavinduchamiran/postharbor/PostHarborApplication.java
+++ backend/RestAPI/WebRestApi/src/me/kavinduchamiran/postharbor/PostHarborApplication.java
@@ -3,9 +3,8 @@ package me.kavinduchamiran.postharbor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {  DataSourceAutoConfiguration.class })
 public class PostHarborApplication {
 
 	public static void main(String[] args) {
